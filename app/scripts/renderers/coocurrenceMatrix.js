7218a177c096f1cb3b8fbc1e96ee7fe90fef7535
Craig Fruin <cfruin@gmail.com>
2016-06-30 23:55:09 -0600

    restructured application using http://yeoman.io/ generator-webapp

---
+++ app/scripts/renderers/coocurrenceMatrix.js
@@ -0,0 +1,312 @@
+var CoocurrenceMatrix = {
+	_data : null,
+	_container : null,
+	_searchText : '',
+	_path : null,
+	_circle : null,
+	_text : null,
+	_searchMatchColor : '#30FF30',
+	_orderSelected : null,
+
+
+	select_json : function(new_json, value) {
+
+	    var margin = {
+	            top: 120,
+	            right: 0,
+	            bottom: 10,
+	            left: 160
+	        },
+	        width = 800,
+	        height = 800;
+
+	    var x = d3.scale.ordinal().rangeBands([0, width]),
+	        z = d3.scale.linear().domain([0, 4]).clamp(true),
+	        c = d3.scale.category10().domain(d3.range(10));
+
+	    var svg = d3.select('d3_plot').append('svg')
+	        .attr('width', width + margin.left + margin.right)
+	        .attr('height', height + margin.top + margin.bottom)
+	        .style('margin-left', '0px')
+	        .append('g')
+	        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+	        // Based on the user-selected input text above, make the appropriate data set call and retrieve the json
+	        d3.json(new_json, function(systems) {
+
+	            //console.log(new_json)
+
+	            var matrix = [],
+	                nodes = systems.nodes,
+	                n = nodes.length;
+
+	            // Compute index per node.
+	            nodes.forEach(function(node, i) {
+	                node.index = i;
+	                node.count = 0;
+	                matrix[i] = d3.range(n).map(function(j) {
+	                    return {
+	                        x: j,
+	                        y: i,
+	                        z: 0
+	                    };
+	                });
+	            });
+
+	            // Convert links to matrix; count integration occurrences.
+	            systems.links.forEach(function(link) {
+	                matrix[link.source][link.target].z += link.value;
+	                matrix[link.target][link.source].z += link.value;
+	                matrix[link.source][link.source].z += link.value;
+	                matrix[link.target][link.target].z += link.value;
+	                nodes[link.source].count += link.value;
+	                nodes[link.target].count += link.value;
+	            });
+
+	            // Precompute the orders.
+	            var orders = {
+	                name: d3.range(n).sort(function(a, b) {
+	                    return d3.ascending(nodes[a].name, nodes[b].name);
+	                }),
+	                count: d3.range(n).sort(function(a, b) {
+	                    return nodes[b].count - nodes[a].count;
+	                }),
+	                group: d3.range(n).sort(function(a, b) {
+	                    return nodes[b].group - nodes[a].group;
+	                })
+	            };
+
+	            // The default sort order is by name.
+	            x.domain(orders.name);
+
+	            svg.append('rect')
+	                .attr('class', 'background')
+	                .attr('width', width)
+	                .attr('height', height);
+
+	            var row = svg.selectAll('.row')
+	                .data(matrix)
+	                .enter().append('g')
+	                .attr('class', 'row')
+	                .attr('transform', function(d, i) {
+	                    return 'translate(0,' + x(i) + ')';
+	                })
+	                .each(row);
+
+	            row.append('line')
+	                .attr('x2', width);
+
+	            row.append('text')
+	                .attr('x', -6)
+	                .attr('y', x.rangeBand() / 2)
+	                .attr('dy', '.32em')
+	                .attr('text-anchor', 'end')
+	                .text(function(d, i) {
+	                    return nodes[i].name;
+	                });
+
+	            var column = svg.selectAll('.column')
+	                .data(matrix)
+	                .enter().append('g')
+	                .attr('class', 'column')
+	                .attr('transform', function(d, i) {
+	                    return 'translate(' + x(i) + ')rotate(-90)';
+	                });
+
+	            column.append('line')
+	                .attr('x1', -width);
+
+	            column.append('text')
+	                .attr('x', 6)
+	                .attr('y', x.rangeBand() / 2)
+	                .attr('dy', '.32em')
+	                .attr('text-anchor', 'start')
+	                .text(function(d, i) {
+	                    return nodes[i].name;
+	                });
+
+	            function row(row) {
+	                var cell = d3.select(this).selectAll('.cell')
+	                    .data(row.filter(function(d) {
+	                        return d.z;
+	                    }))
+	                    .enter().append('rect')
+	                    .attr('class', 'cell')
+	                    .attr('x', function(d) {
+	                        return x(d.x);
+	                    })
+	                    .attr('width', x.rangeBand())
+	                    .attr('height', x.rangeBand())
+	                    .style('fill-opacity', function(d) {
+	                        return z(d.z);
+	                    })
+	                    .style('fill', function(d) {
+	                        return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null;
+	                    })
+	                    .on('mouseover', mouseover)
+	                    .on('mouseout', mouseout);
+	            }
+
+	            function mouseover(p) {
+	                d3.selectAll('.row text').classed('active', function(d, i) {
+	                    return i == p.y;
+	                });
+	                d3.selectAll('.column text').classed('active', function(d, i) {
+	                    return i == p.x;
+	                });
+	            }
+
+	            function mouseout() {
+	                d3.selectAll('text').classed('active', false);
+	            }
+
+	            d3.select('#order').on('change', function() {
+	                clearTimeout(timeout);
+	                order(this.value);
+	            });
+
+							//This is the function that actually orders the nodes on the screen
+	            function order(value) {
+	                x.domain(orders[value]);
+
+	                var t = svg.transition().duration(2500);
+
+	                t.selectAll('.row')
+	                    .delay(function(d, i) {
+	                        return x(i) * 4;
+	                    })
+	                    .attr('transform', function(d, i) {
+	                        return 'translate(0,' + x(i) + ')';
+	                    })
+	                    .selectAll('.cell')
+	                    .delay(function(d) {
+	                        return x(d.x) * 4;
+	                    })
+	                    .attr('x', function(d) {
+	                        return x(d.x);
+	                    });
+
+	                t.selectAll('.column')
+	                    .delay(function(d, i) {
+	                        return x(i) * 4;
+	                    })
+	                    .attr('transform', function(d, i) {
+	                        return 'translate(' + x(i) + ')rotate(-90)';
+	                    });
+	            }
+
+	            var timeout = setTimeout(function() {
+	                order('name');
+	                d3.select('#order').property('selectedIndex', 0).node().focus();
+	            }, 5000);
+	        });
+
+
+	    },
+
+	init : function() {
+
+		CoocurrenceMatrix.select_json('data/design_coocurrence/current_state.js','name');
+
+		var html = new Array();
+		html.push('<div class=\'panel-group\'>');
+		html.push('<div class=\'panel panel-default\'>');
+		html.push('<div class=\'panel-heading\'>System Map Options</div>')
+
+		html.push('<div class=\'panel-body\'>');
+		html.push('<div class=\'form-group\'>');
+		html.push('<label for=\'systemMap\'>System Map:</label>');
+		html.push('<select class=\'form-control\' id=\'selected_json\'>');
+		html.push('<option value=\'data/design_coocurrence/current_state.js\'>Production</option>');
+		html.push('</select>');
+		html.push('</div>');
+		html.push('</div>');
+
+		html.push('</div>');
+		html.push('</div>');
+		html.push('</div>');
+
+		html.push('<div class=\'panel-group\'>');
+		html.push('<div class=\'panel panel-default\'>');
+		html.push('<div class=\'panel-heading\'>Ordering Options</div>')
+
+		html.push('<div class=\'panel-body\'>');
+		html.push('<div class=\'form-group\'>');
+		html.push('<label for=\'ordering\'>Ordering Options:</label>');
+		html.push('<select class=\'form-control\' id=\'order\' onchange=\'CoocurrenceMatrix.refresh()\'>');
+		html.push('<option value=\'name\' ' + CoocurrenceMatrix.setOrder( 'name') + '>Name</option>');
+		html.push('<option value=\'count\' ' + CoocurrenceMatrix.setOrder('count') + '>Frequency</option>');
+		html.push('<option value=\'group\' ' + CoocurrenceMatrix.setOrder('group') + '>Cluster</option>');
+		html.push('</select>');
+		html.push('</div>');
+		html.push('</div>');
+
+		html.push('</div>');
+		html.push('</div>');
+		html.push('</div>');
+		html.push('<p>Each colored cell represents an integration between two systems.');
+		html.push(' Darker cells indicate that there are a larger amount of integrations between these systems.</p>');
+		html.push('<p>Use the drop-down menus to select a different data repository, reorder the matrix, and explore the data.</p>')
+		$('#coccurrenceMatrixInfo').empty().append(html.join(''))
+
+		$('.info').css('display', 'inline-block');
+	},
+	refresh : function() {
+		$('#CoocurrenceMatrixCanvas').empty();
+	},
+	setOrder : function(key, value) {
+		CoocurrenceMatrix.orderSelected = value;
+	},
+	getOrder : function(key, defaultValue) {
+		return CoocurrenceMatrix.orderSelected;
+	},
+	row: function(row) {
+      var cell = d3.select(this).selectAll('.cell')
+          .data(row.filter(function(d) {
+              return d.z;
+          }))
+          .enter().append('rect')
+          .attr('class', 'cell')
+          .attr('x', function(d) {
+              return x(d.x);
+          })
+          .attr('width', x.rangeBand())
+          .attr('height', x.rangeBand())
+          .style('fill-opacity', function(d) {
+              return z(d.z);
+          })
+          .style('fill', function(d) {
+              return nodes[d.x].group == nodes[d.y].group ? c(nodes[d.x].group) : null;
+          })
+          .on('mouseover', mouseover)
+          .on('mouseout', mouseout);
+  },
+	render : function(container, data) {
+		$(container).load('./templates/coocurrenceMatrix.htm?uuid=' + CoocurrenceMatrix.generateUUID(), function() {
+			CoocurrenceMatrix._data = data;
+			CoocurrenceMatrix._container = container;
+			CoocurrenceMatrix.init();
+			CoocurrenceMatrix.refresh();
+		});
+		window.addEventListener('resize', CoocurrenceMatrix.refresh)
+	},
+	setSearchText : function(text) {
+		CoocurrenceMatrix._searchText = text;
+	},
+	setState : function(key, value) {
+		if (window.localStorage) window.localStorage[key] = value;
+	},
+	getState : function(key, defaultValue) {
+		return window.localStorage ? window.localStorage[key] : defaultValue ? defaultValue : '';
+	},
+	generateUUID : function() {
+		var d = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (d + Math.random()*16)%16 | 0;
+			d = Math.floor(d/16);
+			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+		});
+		return uuid;
+	}
+
+}
---
+++ app/scripts/renderers/logicalDiagram.js
@@ -0,0 +1,260 @@
+var LogicalDiagram = {
+	_data : null,
+	_container : null,
+	_graph : ['options','links','groups'],
+	_searchText : '',
+	_path : null,
+	_circle : null,
+	_text : null,
+	_searchMatchColor : '#30FF30',
+	init : function() {
+		LogicalDiagram._graph.links = new Array();
+		LogicalDiagram._graph.groups = new Array();
+		LogicalDiagram._graph.options = new Array();
+
+		// Get list of all endpoints for all components
+		var servicesList = [];
+		for (var i=0;i<LogicalDiagram._data.components.length;i++) {
+			var component = LogicalDiagram._data.components[i];
+			component.nodeSizeNone = 0;
+			component.nodeSizeDependencies = 0;
+			component.nodeSizeDependents = 0;
+			if (component.services) {
+				for (var j=0;j<component.services.length;j++) {
+					servicesList.push(component.services[j]);
+				}
+			}
+		}
+
+		for (var i=0;i<LogicalDiagram._data.components.length;i++)
+		{
+			var component = LogicalDiagram._data.components[i];
+			if (component.dependsOn) {
+				for (var j=0;j<component.dependsOn.length;j++) {
+					component.nodeSizeDependencies = component.dependsOn.length;
+					LogicalDiagram._data.components.find(function(dependComponent) {
+						// once the dependenent component is found
+						if (component.dependsOn[j].endpointId.startsWith(dependComponent.acronym)) {
+							// reference the actual service
+							var serviceDefinition = servicesList.find(function(s) {
+								return s.id === component.dependsOn[j].endpointId;
+							});
+
+							// Using the target component, create a relationship between the two (source, target) so we can graph
+							var t = LogicalDiagram._data.components.indexOf(dependComponent);
+							LogicalDiagram._graph.links.pushIfUnique({source:i, target:t, service:serviceDefinition}, function(e) {
+									return LogicalDiagram._data.components[e.source].acronym === component.acronym &&
+										LogicalDiagram._data.components[e.target].acronym === dependComponent.acronym;
+							});
+							dependComponent.nodeSizeDependents += 1;
+						}
+					});
+				}
+			}
+		}
+
+		LogicalDiagram._graph.options.stackHeight = 12;
+		LogicalDiagram._graph.options.radius = 6;
+		LogicalDiagram._graph.options.fontSize = 12;
+		LogicalDiagram._graph.options.labelFontSize = 8;
+		LogicalDiagram._graph.options.nodeLabel = 'acronym';
+		LogicalDiagram._graph.options.nodeDescription = 'name';
+		LogicalDiagram._graph.options.color = 'color';
+		LogicalDiagram._graph.options.markerWidth = 6;
+		LogicalDiagram._graph.options.markerHeight = 6;
+		LogicalDiagram._graph.options.gap = 1.5;
+		LogicalDiagram._graph.options.linkDistance = 240;
+		LogicalDiagram._graph.options.charge = -720;
+		LogicalDiagram._graph.options.styleColumn = 'endpointType';
+		LogicalDiagram._graph.options.styles = 'endpointType';
+		LogicalDiagram._graph.options.linkName = 'description';
+	},
+	refresh : function() {
+		$('#logicalDiagramCanvas').empty();
+		var graph = LogicalDiagram._graph;
+		var options = graph.options;
+
+		options.width = $(window).height() - 60;
+		options.height = options.width;
+
+		var force = d3.layout.
+			force().
+			nodes(LogicalDiagram._data.components).
+			links(graph.links).
+			size([options.width, options.height]).
+			linkDistance(options.linkDistance).
+			charge(options.charge).
+			on('tick', LogicalDiagram.tick).start();
+		var svg = d3.select('#logicalDiagramCanvas').
+			append('svg:svg').
+			attr('width', options.width).
+			attr('height', options.height);
+
+		var color = d3.scale.category20();
+
+		var linkStyles = [];
+	    if (options.styleColumn) {
+			var x;
+			for (var i = 0; i < graph.links.length; i++) {
+				if (linkStyles.indexOf( x = graph.links[i].service[options.styleColumn].toLowerCase()) == -1) linkStyles.push(x);
+			}
+		} else linkStyles[0] = 'defaultMarker';
+
+		if (options.markerWidth) {
+			svg.append('svg:defs').
+				selectAll('marker').
+				data(linkStyles).enter().
+				append('svg:marker').attr('id', String).
+				attr('viewBox', '0 -5 10 10').
+				attr('refX', 15).attr('refY', -1.5).
+				attr('markerWidth', options.markerWidth).
+				attr('markerHeight', options.markerHeight).
+				attr('orient', 'auto').
+				append('svg:path').
+				attr('d', 'M0,-5L10,0L0,5');
+		}
+
+		LogicalDiagram._path = svg.append('svg:g').
+			selectAll('path').
+			data(force.links()).
+			enter().
+			append('svg:path').
+			attr('class', function(d) {
+				return 'link ' + (options.styleColumn ? d.service[options.styleColumn].toLowerCase() : linkStyles[0]);
+			}).attr('marker-end', function(d) {
+				return 'url(#' + (options.styleColumn ? d.service[options.styleColumn].toLowerCase() : linkStyles[0] ) + ')';
+			});
+
+		LogicalDiagram._circle = svg.append('svg:g').
+			selectAll('circle').
+			data(force.nodes()).enter().
+			append('svg:circle').attr('r', function(d) {
+				return LogicalDiagram.getRadius(d);
+			}).style('fill', function(d) {
+				if (d.services && d.services.length > 0) {
+					for (var j=0;j<d.services.length;j++) {
+						if (LogicalDiagram.findSearchMatchesForService(d.services[j]))
+							return LogicalDiagram._searchMatchColor;
+					}
+				}
+				return d[options.color];
+			}).call(force.drag);
+
+		if (options.nodeDescription) { LogicalDiagram._circle.append('title').text(
+			function(d) {
+				return d[options.nodeDescription] +
+					'\nDepends on ' + (d.nodeSizeDependencies ? d.nodeSizeDependencies : '0') + ' service(s)' +
+					'\nIs referenced ' + (d.nodeSizeDependents ? d.nodeSizeDependents : '0') + ' times(s)';
+			});
+		}
+    	if (options.linkName) {	LogicalDiagram._path.append('title').text(
+			function(d) {
+				return d.service[options.linkName];	// should return # of dependencies on that component instead
+			});
+		}
+		LogicalDiagram._text = svg.append('svg:g').selectAll('g').data(force.nodes()).enter().append('svg:g');
+		LogicalDiagram._text.append('svg:text').
+			attr('x', options.labelFontSize).
+			attr('y', '.31em').
+			attr('class', 'shadow').text(function(d) { return d[options.nodeLabel]; });
+		LogicalDiagram._text.append('svg:text').
+			attr('x', options.labelFontSize).
+			attr('y', '.31em').text(function(d) { return d[options.nodeLabel]; });
+
+
+		var html = new Array();
+		html.push('<div class=\'panel-group\'>');
+		html.push('<div class=\'panel panel-default\'>');
+		html.push('<div class=\'panel-heading\'>Diagram Options</div>')
+
+		html.push('<div class=\'panel-body\'>');
+		html.push('<div class=\'form-group\'>');
+		html.push('<label for=\'componentSize\'>Component size based on:</label>');
+		html.push('<select class=\'form-control\' id=\'componentSize\' onchange=\'LogicalDiagram.handleComponentSize(this)\'>');
+		html.push('<option value=\'None\' ' + LogicalDiagram.isSelected('None') + '>None</option>');
+		html.push('<option value=\'Dependencies\' ' + LogicalDiagram.isSelected('Dependencies') + '># of Dependencies</option>');
+		html.push('<option value=\'Dependents\'' + LogicalDiagram.isSelected('Dependents') + '># of Dependents</option>');
+		html.push('</select>');
+		html.push('</div>');
+		html.push('</div>');
+
+		html.push('</div>');
+		html.push('</div>');
+		html.push('</div>');
+		$('#logicalDiagramInfo').empty().append(html.join(''))
+
+		$('.info').css('display', 'inline-block');
+
+	},
+	render : function(container, data) {
+		$(container).load('./templates/logicalDiagram.htm?uuid=' + LogicalDiagram.generateUUID(), function() {
+			LogicalDiagram._data = data;
+			LogicalDiagram._container = container;
+			LogicalDiagram.init();
+			LogicalDiagram.refresh();
+		});
+		window.addEventListener('resize', LogicalDiagram.refresh)
+	},
+	getRadius : function(d) {
+		var options = LogicalDiagram._graph.options;
+		return options.radius * (LogicalDiagram.getState('componentSizeIndicator') ? Math.sqrt((d['nodeSize' + LogicalDiagram.getState('componentSizeIndicator', 'None')] + 1) * 10) / Math.PI : 1);
+    },
+	tick : function() {
+		LogicalDiagram._path.attr('d', function(d) {
+			var dx = d.target.x - d.source.x, dy = d.target.y - d.source.y, dr = Math.sqrt(dx * dx + dy * dy);
+			return 'M' + d.source.x + ',' + d.source.y + 'A' + dr + ',' + dr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
+		});
+
+		LogicalDiagram._circle.attr('transform', function(d) {
+			return 'translate(' + d.x + ',' + d.y + ')';
+		});
+
+		LogicalDiagram._text.attr('transform', function(d) {
+			return 'translate(' + d.x + ',' + d.y + ')';
+		});
+	},
+	handleComponentSize : function(element) {
+		LogicalDiagram.setState('componentSizeIndicator', element.value);
+		LogicalDiagram.refresh();
+	},
+	isSelected : function(componentSizeIndicator) {
+		return (componentSizeIndicator === LogicalDiagram.getState('componentSizeIndicator', 'None')) ? ' selected' : '';
+	},
+	findSearchMatchesForService : function(service) {
+		// search id, description, endpointSig and tags
+		if (LogicalDiagram._searchText.length > 2) {
+			if (LogicalDiagram.findSearchMatch([service.id])) return true;
+			if (LogicalDiagram.findSearchMatch([service.description])) return true;
+			if (LogicalDiagram.findSearchMatch([service.endpointSig])) return true;
+			if (LogicalDiagram.findSearchMatch(service.tags)) return true;
+		}
+		return false;
+	},
+	findSearchMatch : function(list) {
+		if (list && list.length > 0)
+			return list.find(function(e) { return e.indexOfInsensitive(LogicalDiagram._searchText) > -1; });
+		else return false;
+	},
+	setSearchText : function(text) {
+		LogicalDiagram._searchText = text;
+	},
+	searchFor : function(text) {
+		LogicalDiagram.setSearchText(text);
+		LogicalDiagram.refresh();
+	},
+	setState : function(key, value) {
+		if (window.localStorage) window.localStorage[key] = value;
+	},
+	getState : function(key, defaultValue) {
+		return window.localStorage ? window.localStorage[key] : defaultValue ? defaultValue : '';
+	},
+	generateUUID : function() {
+		var d = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = (d + Math.random()*16)%16 | 0;
+			d = Math.floor(d/16);
+			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+		});
+		return uuid;
+	}
+}
